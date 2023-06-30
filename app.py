9025ad13928639556bf657370f61818c72bf0e6f
ami25Ha <107530505+ami25Ha@users.noreply.github.com>
2023-06-30 11:07:58 +0000

    added the flask project

--- app.py
+++ app.py
@@ -8,3 +8,7 @@ def hello_world(): #defining a function
 
 if __name__=='__main__':
   app.run(host='0.0.0.0',debug=True) #local host
+#1-Create a project on Github
+#2-open up the project on replit
+#3-create and run a flask web server
+#4-push changes back to Github
