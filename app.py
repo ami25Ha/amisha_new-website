092d9718d5abbabb73388a65277ff2aa20ff1a56
ami25Ha <107530505+ami25Ha@users.noreply.github.com>
2023-07-02 11:15:11 +0000

    added html and css

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template#to use template we use render template function.
 
 app=Flask(__name__) #created flask applicaation
 
 @app.route("/")#now create a route
 def hello_world(): #defining a function
-  return "Hello World"
+  return render_template("home.html") #retrun template
 
 if __name__=='__main__':
-  app.run(host='0.0.0.0',debug=True) #local host
+  app.run(host='0.0.0.0',debug=True) #local host (host=0.0.0.0)
 #1-Create a project on Github
 #2-open up the project on replit
 #3-create and run a flask web server
